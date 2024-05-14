e5c7b87339f695377d3c517405c7cc57dd0c2b06
Taggartjoey9 <taggartjoey9@gmail.com>
2024-05-14 08:53:43 -0400

    configuring game board structure along with styling

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1>
-      Tic-Tac-Toe Game
-    </h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>  
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>  
+            <span className="player-symbol">O</span>
+          </li>    
+        </ol> 
+        GAME BOARD
+      </div>    
+      LOG 
+    </main>
   );
 }
 
