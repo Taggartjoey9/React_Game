78954958fd59e5197f7833702f1d923bca567ea8
Taggartjoey9 <taggartjoey9@gmail.com>
2024-05-18 22:51:31 -0400

    added two way binding to update player name with save/edit btn

--- tictactoe/src/components/Player.js
+++ tictactoe/src/components/Player.js
@@ -1,26 +1,31 @@
 import { useState } from 'react';
 
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+
+    const [playerName, setPlayerName] = useState(initialName)
 
     const [isEditing, setIsEditing] = useState(false)
 
-    function handleEditClick() {
-        setIsEditing(!isEditing)
+    function handleUpdatedName(event) {
+        setPlayerName(event.target.value)
     }
 
-    let playerName = <span className="player-name">{name}</span>  
+    function handleEditClick() {
+        setIsEditing((isEditing) => !isEditing)
+    }
+
+    let editPlayerName = <span className="player-name">{playerName}</span>  
 
     if(isEditing) {
-        playerName = <input type='text' required value={name}/>
+        editPlayerName = <input type='text' required value={playerName} onChange={handleUpdatedName}/>
     }
-
 
 
     return (
         <li>
             <span className="player">
-              {playerName}
+              {editPlayerName}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
